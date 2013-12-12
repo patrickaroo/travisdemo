6c5cb1ac299ecd4fbfb116e4e1e3c6e14d1471f4
Michael Hrivnak <mhrivnak@redhat.com>
2013-12-12 16:28:38 -0500

    adding a getter function for the recipient, which is a terrible idea, but helps my demo

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -6,3 +6,7 @@ class TestHello(unittest.TestCase):
     def test_greeting(self):
         message = travisdemo.hello()
         self.assertTrue(message.lower().startswith('hello'))
+
+    def test_recipient(self):
+        message = travisdemo.hello()
+        self.assertTrue(message.endswith('TriLUG!'))
--- travisdemo/__init__.py
+++ travisdemo/__init__.py
@@ -1,2 +1,6 @@
 def hello():
-    return 'hello, world!'
+    return 'hello, %s!' % get_recipient
+
+
+def get_recipient():
+    return 'TriLUG'
